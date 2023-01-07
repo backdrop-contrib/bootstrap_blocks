1b238a03685c4d1e4272fa4dc0476e610ff32bff
docwilmot <docwilmot@users.noreply.github.com>
2023-01-07 12:35:40 -0500

    Make block ID general

--- templates/tabs-vertical.tpl.php
+++ templates/tabs-vertical.tpl.php
@@ -1,10 +1,9 @@
 <?php $tabs_padding = ($tabs_padding > 0) ? 'padding: ' . $tabs_padding . 'px': ''; ?>
 <?php dpm(get_defined_vars());  $tabs_margin = ($tabs_margin > 0) ? 'margin: ' . $tabs_margin . 'px' : ''; ?>
-<?php $tab_id = !empty($tab_id) ? backdrop_clean_css_identifier($tab_id) : 'vtab-block-' . $id; ?>
 
-<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $tab_id; ?> d-flex align-items-start border">
+<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $bblock_id; ?> d-flex align-items-start border">
   
-  <div class="nav nav-pills flex-column" id="<?php echo $tab_id; ?>" role="tablist">
+  <div class="nav nav-pills flex-column" id="<?php echo $bblock_id; ?>" role="tablist">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'active' : ''; ?>
       <button class="nav-link <?php echo $active; ?>" id="tabv-<?php echo $delta; ?>" data-bs-toggle="tab" data-bs-target="#tabv-content-<?php echo $delta; ?>" role="tab" type="button" aria-controls="tabv-content-<?php echo $delta; ?>" aria-selected="true">
@@ -13,7 +12,7 @@
     <?php endforeach; ?>
   </div>
 
-  <div class="tab-content" id="<?php echo $tab_id; ?>Content">
+  <div class="tab-content" id="<?php echo $bblock_id; ?>Content">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'show active' : ''; ?>
     <div class="tab-pane p-4 fade <?php echo $active; ?>" id="tabv-content-<?php echo $delta; ?>" role="tabpanel" aria-labelledby="tabv-content-<?php echo $delta; ?>-tab"><?php echo $item['content']; ?></div>
--- templates/tabs.tpl.php
+++ templates/tabs.tpl.php
@@ -1,11 +1,10 @@
 <?php $tabs_padding = ($tabs_padding > 0) ? 'padding: ' . $tabs_padding . 'px': ''; ?>
 <?php $tabs_margin = ($tabs_margin > 0) ? 'margin: ' . $tabs_margin . 'px' : ''; ?>
 <?php dpm(get_defined_vars());  $link_style_class = ($link_style == 'buttons') ? 'nav-pills' : 'nav-tabs'; ?>
-<?php $tab_id = !empty($tab_id) ? backdrop_clean_css_identifier($tab_id) : 'vtab-block-' . $id; ?>
 
-<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $tab_id; ?> border">
+<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $bblock_id; ?> border">
   
-  <div class="nav <?php echo $link_style_class; ?>" id="<?php echo $tab_id; ?>" role="tablist">
+  <div class="nav <?php echo $link_style_class; ?>" id="<?php echo $bblock_id; ?>" role="tablist">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'active' : ''; ?>
       <button class="nav-link <?php echo $active; ?>" id="tab-<?php echo $delta; ?>" data-bs-toggle="tab" data-bs-target="#tab-content-<?php echo $delta; ?>" role="tab" type="button" aria-controls="tab-content-<?php echo $delta; ?>" aria-selected="true">
@@ -14,7 +13,7 @@
     <?php endforeach; ?>
   </div>
 
-  <div class="tab-content" id="<?php echo $tab_id; ?>Content">
+  <div class="tab-content" id="<?php echo $bblock_id; ?>Content">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'show active' : ''; ?>
     <div class="tab-pane p-4 fade <?php echo $active; ?>" id="tab-content-<?php echo $delta; ?>" role="tabpanel" aria-labelledby="tab-content-<?php echo $delta; ?>-tab"><?php echo $item['content']; ?></div>
