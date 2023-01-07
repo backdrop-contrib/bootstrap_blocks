eb0bd4d81d9b7e7aa28f8a49f59f75c1ffb2b0c8
docwilmot <docwilmot@users.noreply.github.com>
2023-01-06 23:54:55 -0500

    Split again into vtabs and tabs

--- templates/tabs-vertical.tpl.php
+++ templates/tabs-vertical.tpl.php
@@ -1,81 +1,23 @@
-<div class="bd-example-tabs" style="
-  <?php if (($tabs_padding > 0)) { ?>
- padding: <?php echo $tabs_padding; ?>px; <?php } ?>
- 
-  <?php if (($tabs_margin > 0)) { ?>
- margin:  <?php echo $tabs_margin; ?>px;  <?php } ?>
-">
+<?php $tabs_padding = ($tabs_padding > 0) ? 'padding: ' . $tabs_padding . 'px': ''; ?>
+<?php dpm(get_defined_vars());  $tabs_margin = ($tabs_margin > 0) ? 'margin: ' . $tabs_margin . 'px' : ''; ?>
+<?php $tab_id = !empty($tab_id) ? backdrop_clean_css_identifier($tab_id) : 'vtab-block-' . $id; ?>
+
+<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $tab_id; ?> d-flex align-items-start border">
   
-  <div class="row">
-    <div class="col-4 col-md-3">
-      <ul class="nav nav-tabs nav-tabs-vertical <?php if ($tab_background) { ?>
- nav-tabs-vertical-background <?php } ?>
-" id="myTab" role="tablist" aria-orientation="vertical">
-        <li class="nav-item">
-          <a class="nav-link <?php if ($first_tab_active) { ?>
- active <?php } else { ?>
- disabled <?php } ?>
-" id="<?php echo ($tab_id . '1'); ?>-tab" data-toggle="tab" href="#<?php echo ($tab_id . '1'); ?>" role="tab" aria-controls="<?php echo ($tab_id . '1'); ?>" aria-selected="<?php if ($first_tab_active) { ?>
- true <?php } else { ?>
- false <?php } ?>
-">
-            <?php echo $first_tab_title; ?>
-            <?php if ($first_tab_icon) { ?>
-              <svg class="icon icon-primary">
-                <use xlink:href="./themes/custom/italiagov/assets/icons/sprite.svg#<?php echo $first_tab_icon; ?>"></use>
-              </svg>
-            <?php } ?>
-          </a>
-        </li>
+  <div class="nav nav-pills flex-column" id="<?php echo $tab_id; ?>" role="tablist">
+  <?php foreach ($tabs as $delta => $item): ?>
+    <?php $active = ($delta == 0) ? 'active' : ''; ?>
+      <button class="nav-link <?php echo $active; ?>" id="tabv-<?php echo $delta; ?>" data-bs-toggle="tab" data-bs-target="#tabv-content-<?php echo $delta; ?>" role="tab" type="button" aria-controls="tabv-content-<?php echo $delta; ?>" aria-selected="true">
+      <?php echo $item['title']; ?>
+      </button>
+    <?php endforeach; ?>
+  </div>
 
-        <li class="nav-item">
-          <a class="nav-link <?php if (($second_tab_active &&  !$first_tab_active)) { ?>
- active <?php } elseif ( !$second_tab_active) { ?>
- disabled <?php } ?>
-" id="<?php echo ($tab_id . '2'); ?>-tab" data-toggle="tab" href="#<?php echo ($tab_id . '2'); ?>" role="tab" aria-controls="<?php echo ($tab_id . '1'); ?>" aria-selected="<?php if (($second_tab_active &&  !$first_tab_active)) { ?>
- true <?php } else { ?>
- false <?php } ?>
-">
-            <?php echo $second_tab_title; ?>
-            <?php if ($second_tab_icon) { ?>
-              <svg class="icon icon-primary">
-                <use xlink:href="./themes/custom/italiagov/assets/icons/sprite.svg#<?php echo $second_tab_icon; ?>"></use>
-              </svg>
-            <?php } ?>
-          </a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link <?php if (($third_tab_active && ( !$second_tab_active &&  !$first_tab_active))) { ?>
- active <?php } elseif ( !$third_tab_active) { ?>
- disabled <?php } ?>
-" id="<?php echo ($tab_id . '3'); ?>-tab" data-toggle="tab" href="#<?php echo ($tab_id . '3'); ?>" role="tab" aria-controls="<?php echo ($tab_id . '3'); ?>" aria-selected="<?php if (($third_tab_active && ( !$second_tab_active &&  !$first_tab_active))) { ?>
- true <?php } else { ?>
- false <?php } ?>
-">
-            <?php echo $third_tab_title; ?>
-            <?php if ($third_tab_icon) { ?>
-              <svg class="icon icon-primary">
-                <use xlink:href="./themes/custom/italiagov/assets/icons/sprite.svg#<?php echo $third_tab_icon; ?>"></use>
-              </svg>
-            <?php } ?>
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="col-8 col-md-9">
-      <div class="tab-content" id="myTabContent">
-        <div class="tab-pane p-4 fade <?php if ($first_tab_active) { ?>
- show active <?php } ?>
-" id="<?php echo ($tab_id . '1'); ?>" role="tabpanel" aria-labelledby="<?php echo ($tab_id . '1'); ?>-tab"><?php echo $first_tab_content; ?></div>
-        <div class="tab-pane p-4 fade <?php if (($second_tab_active &&  !$first_tab_active)) { ?>
- show active <?php } ?>
-" id="<?php echo ($tab_id . '2'); ?>" role="tabpanel" aria-labelledby="<?php echo ($tab_id . '2'); ?>-tab"><?php echo $second_tab_content; ?></div>
-        <div class="tab-pane p-4 fade <?php if (($third_tab_active && ( !$second_tab_active &&  !$first_tab_active))) { ?>
- show active <?php } ?>
-" id="<?php echo ($tab_id . '3'); ?>" role="tabpanel" aria-labelledby="<?php echo ($tab_id . '3'); ?>-tab"><?php echo $third_tab_content; ?></div>
-      </div>
-    </div>
+  <div class="tab-content" id="<?php echo $tab_id; ?>Content">
+  <?php foreach ($tabs as $delta => $item): ?>
+    <?php $active = ($delta == 0) ? 'show active' : ''; ?>
+    <div class="tab-pane p-4 fade <?php echo $active; ?>" id="tabv-content-<?php echo $delta; ?>" role="tabpanel" aria-labelledby="tabv-content-<?php echo $delta; ?>-tab"><?php echo $item['content']; ?></div>
+    <?php endforeach; ?>
   </div>
 </div>
+
--- templates/tabs.tpl.php
+++ templates/tabs.tpl.php
@@ -1,18 +1,11 @@
 <?php $tabs_padding = ($tabs_padding > 0) ? 'padding: ' . $tabs_padding . 'px': ''; ?>
 <?php $tabs_margin = ($tabs_margin > 0) ? 'margin: ' . $tabs_margin . 'px' : ''; ?>
-<?php if ($v_tabs) {
-    $v_tabs_align = 'class="d-flex align-items-start"';
-    $v_tabs_column = 'flex-column';
-  }
-  else {
-    $v_tabs_align = '';
-    $v_tabs_column = '';
-  }
-  ?>
+<?php dpm(get_defined_vars());  $link_style_class = ($link_style == 'buttons') ? 'nav-pills' : 'nav-tabs'; ?>
+<?php $tab_id = !empty($tab_id) ? backdrop_clean_css_identifier($tab_id) : 'vtab-block-' . $id; ?>
 
-<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" <?php echo $v_tabs_align; ?>>
+<div style="<?php echo $tabs_padding; ?> <?php echo $tabs_margin; ?>" class="<?php echo $tab_id; ?> border">
   
-  <div class="nav nav-pills <?php echo $v_tabs_column; ?>" id="myTab" role="tablist">
+  <div class="nav <?php echo $link_style_class; ?>" id="<?php echo $tab_id; ?>" role="tablist">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'active' : ''; ?>
       <button class="nav-link <?php echo $active; ?>" id="tab-<?php echo $delta; ?>" data-bs-toggle="tab" data-bs-target="#tab-content-<?php echo $delta; ?>" role="tab" type="button" aria-controls="tab-content-<?php echo $delta; ?>" aria-selected="true">
@@ -21,7 +14,7 @@
     <?php endforeach; ?>
   </div>
 
-  <div class="tab-content" id="myTabContent">
+  <div class="tab-content" id="<?php echo $tab_id; ?>Content">
   <?php foreach ($tabs as $delta => $item): ?>
     <?php $active = ($delta == 0) ? 'show active' : ''; ?>
     <div class="tab-pane p-4 fade <?php echo $active; ?>" id="tab-content-<?php echo $delta; ?>" role="tabpanel" aria-labelledby="tab-content-<?php echo $delta; ?>-tab"><?php echo $item['content']; ?></div>
