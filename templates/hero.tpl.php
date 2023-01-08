b4d796762bc2501c3df245a8250481ce22575edb
docwilmot <docwilmot@users.noreply.github.com>
2023-01-08 13:47:34 -0500

    Remove default text

--- templates/hero.tpl.php
+++ templates/hero.tpl.php
@@ -1,4 +1,4 @@
-<div class="card bg-dark text-white rounded-<?php echo $card_round; ?> <?php echo $bblock_id; ?> m-<?php echo $card_margin; ?> <?php echo $text_align; ?>">
+<div class="card text-white rounded-<?php echo $card_round; ?> <?php echo $bblock_id; ?> m-<?php echo $card_margin; ?> <?php echo $text_align; ?>">
   <img class="rounded-<?php echo $card_round; ?>" src="<?php echo $image_path; ?>" class="card-img-top" alt="...">
   <div class="card-img-overlay rounded-<?php echo $card_round; ?> <?php echo $bblock_id; ?> m-<?php echo $card_margin; ?> p-<?php echo $card_padding; ?> <?php echo $text_align; ?>">
     <?php if ($card_header): ?>
